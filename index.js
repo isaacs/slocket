b25cd025c206f6e478174fe5d0678e91f007c5cc
isaacs <i@izs.me>
2017-01-29 23:16:47 -0800

    more tests

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ module.exports = Slocket
 
 var rimraf = require('rimraf')
 var assert = require('assert')
+var EE = require('events')
 var net = require('net')
 var fs = require('fs')
 var path = require('path')
 var onExit = require('signal-exit')
 var locks = Object.create(null)
+var ID = 0
 
 /* istanbul ignore if */
 if (typeof Promise === undefined)
   Promise = require('bluebird')
 
 var util = require('util')
-util.inherits(Slocket, Promise)
+util.inherits(Slocket, EE)
 
 var debug = function () {}
 
@@ -22,7 +24,7 @@ if (/\bslocket\b/i.test(process.env.NODE_DEBUG || '')) {
   debug = function () {
     var msg = util.format.apply(util, arguments)
     var n = path.basename(this.name)
-    var p = 'SLOCKET:' + process.pid + ':' + n + ' '
+    var p = 'SLOCKET:' + process.pid + ':' + n + ':' + this.id + ' '
     msg = p + msg.trimRight().split('\n').join('\n' + p)
     console.error(msg)
   }
@@ -32,6 +34,7 @@ function Slocket (name, cb) {
   if (!(this instanceof Slocket))
     return new Slocket(name, cb)
 
+  this.id = ID++
   this.name = path.resolve(name)
   if (cb)
     this.cb = cb
@@ -40,7 +43,6 @@ function Slocket (name, cb) {
   this.watcher = null
   this.has = false
   this.had = false
-  this.acquire()
   this.connectionQueue = []
   this.currentClient = null
 
@@ -50,6 +52,8 @@ function Slocket (name, cb) {
   }.bind(this))
   this.then = this.promise.then.bind(this.promise)
   this.catch = this.promise.catch.bind(this.promise)
+
+  this.acquire()
 }
 
 Slocket.prototype.debug = debug
@@ -57,6 +61,7 @@ Slocket.prototype.debug = debug
 Slocket.prototype.cb = function () {}
 
 Slocket.prototype.acquire = function () {
+  this.debug('acquire')
   this.unwatch()
   this.disconnect()
   this.server = net.createServer(this.onServerConnection.bind(this))
@@ -66,10 +71,12 @@ Slocket.prototype.acquire = function () {
 }
 
 Slocket.prototype.onAcquire = function () {
+  this.debug('onAcquire')
   this.unwatch()
   assert.equal(this.has, false)
   this.has = true
   this.had = true
+  this.emit('acquire')
   this.cb(null, this)
   // Promises are sometimes a little clever
   // when you resolve(<Promise>), it hooks onto the .then method
@@ -85,19 +92,24 @@ Slocket.prototype.onAcquire = function () {
 }
 
 Slocket.prototype.onServerListen = function () {
-  process.nextTick(function () {
-    this.debug('onServerListen', this.server.listening)
-    onExit(this.onProcessExit.bind(this))
+  this.debug('onServerListen', this.server.listening)
+  this.emit('serverListen')
+  process.nextTick(function onServerListenNT () {
+    this.debug('onServerListenNT', this.server.listening)
+    this.on('serverClose', onExit(this.onProcessExit.bind(this)))
     this.onAcquire()
   }.bind(this))
 }
 
 Slocket.prototype.onProcessExit = function () {
+  this.debug('onProcessExit')
   if (this.has === true)
     this.release(true)
 }
 
 Slocket.prototype.onServerConnection = function (c) {
+  this.debug('onServerConnection')
+  this.emit('serverConnection', c)
   c.on('close', this.onServerConnectionClose.bind(this, c))
   if (this.currentClient || this.has)
     this.connectionQueue.push(c)
@@ -107,6 +119,7 @@ Slocket.prototype.onServerConnection = function (c) {
 
 Slocket.prototype.onServerConnectionClose = function (c) {
   this.debug('onServerConnectionClose', this.has)
+  this.emit('serverConnectionClose', c)
   if (this.currentClient === c) {
     this.currentClient = null
     this.release()
@@ -118,6 +131,7 @@ Slocket.prototype.onServerConnectionClose = function (c) {
 }
 
 Slocket.prototype.delegate = function (c) {
+  this.debug('delegate')
   assert.equal(this.has, false)
   this.debug('delegate new client')
   this.currentClient = c
@@ -132,8 +146,8 @@ Slocket.prototype.type = function () {
 }
 
 Slocket.prototype.release = function (sync) {
+  this.debug('release has=%j sync=%j', this.has, sync)
   this.has = false
-  this.debug('release')
   this.connectionRelease(sync)
   this.serverRelease(sync)
 }
@@ -151,11 +165,14 @@ Slocket.prototype.serverRelease = function (sync) {
 }
 
 Slocket.prototype.onServerClose = function () {
+  this.debug('onServerClose')
+  this.emit('serverClose')
   this.server = null
 }
 
 Slocket.prototype.onServerError = function (er) {
   this.debug('onServerError', er.message)
+  this.emit('serverError', er)
   // XXX just in the off chance this happens later, kill any connections
   // and destroy the server
   if (this.server)
@@ -174,11 +191,13 @@ Slocket.prototype.onServerError = function (er) {
 }
 
 Slocket.prototype.onError = function (er) {
+  this.debug('onError', er.message)
   this.cb(er, this)
   this.reject(er)
 }
 
 Slocket.prototype.connect = function () {
+  this.debug('connect')
   this.connection = net.createConnection(this.name)
   this.connection.slocketBuffer = ''
   this.connection.setEncoding('utf8')
@@ -189,6 +208,8 @@ Slocket.prototype.connect = function () {
 }
 
 Slocket.prototype.onConnectionData = function (chunk) {
+  this.debug('onConnectionData %s', chunk, this.connection.slocketBuffer)
+  this.emit('connectionData', chunk)
   this.connection.slocketBuffer += chunk
 
   if (this.connection.slocketBuffer === 'OK')
@@ -199,6 +220,8 @@ Slocket.prototype.onConnectionData = function (chunk) {
 }
 
 Slocket.prototype.onConnectionError = function (er) {
+  this.debug('onConnectionError', er.message)
+  this.emit('connectionError', er)
   if (this.has)
     return this.onError(er)
   this.connection = null
@@ -222,11 +245,15 @@ Slocket.prototype.onConnectionError = function (er) {
 }
 
 Slocket.prototype.onConnect = function () {
+  this.debug('onConnect')
+  this.emit('connect')
   this.connection.slocketConnected = true
   this.connection.on('close', this.onConnectionClose.bind(this))
 }
 
 Slocket.prototype.onConnectionClose = function () {
+  this.debug('onConnectionClose')
+  this.emit('connectionClose')
   this.connection.slocketConnected = false
   if (!this.had)
     this.acquire()
@@ -235,6 +262,8 @@ Slocket.prototype.onConnectionClose = function () {
 Slocket.prototype.connectionRelease = function (sync) {
   if (!this.connection)
     return
+
+  this.debug('connectionRelease', sync)
 
   if (this.connection.slocketConnected)
     this.connection.destroy()
@@ -245,18 +274,21 @@ Slocket.prototype.connectionRelease = function (sync) {
 }
 
 Slocket.prototype.disconnect = function () {
+  this.debug('disconnect')
   if (this.connection)
     this.connection.destroy()
   this.connection = null
 }
 
 Slocket.prototype.watch = function () {
+  this.debug('watch')
   this.watcher = fs.watch(this.name, { persistent: false })
   this.watcher.on('change', this.acquire.bind(this))
   this.watch.on('error', this.acquire.bind(this))
 }
 
 Slocket.prototype.unwatch = function () {
+  this.debug('unwatch')
   if (this.watcher)
     this.watcher.close()
   this.watcher = null
--- test/basic.js
+++ test/basic.js
@@ -82,8 +82,81 @@ t.test('3 parallel locks', function (t) {
 })
 
 // these would get up to or near 100% coverage
-t.test('3 serial locks')
-t.test('open 1, open 2, close 1, close 2, open 3, close 3')
+t.test('3 serial locks', function (t) {
+  clear('3-serial')
+  var file = filename('3-serial')
+  t.teardown(clear.bind(null, '3-serial'))
+  function go (i) {
+    if (i === 3)
+      return t.end()
+    Slocket(file, function (er, lock) {
+      if (er)
+        throw er
+      t.pass('got lock ' + i)
+      lock.release()
+      go(i+1)
+    })
+  }
+  go(0)
+})
+
+t.test('staggered', function (t) {
+  clear('3-staggered')
+  var file = filename('3-staggered')
+  t.teardown(clear.bind(null, '3-staggered'))
+
+  var set = []
+  Slocket(file).then(function (lock) {
+    set[0] = lock
+    t.equal(lock.type(), 'server')
+    Slocket(file, function (er, lock) {
+      t.equal(lock.type(), 'connection')
+      set[1] = lock
+
+      Slocket(file, function (er, lock) {
+        t.equal(lock.type(), 'connection')
+        lock.release()
+        t.end()
+      })
+      setTimeout(function () {
+        lock.release()
+      }, 100)
+    }).on('connect', function () {
+      lock.release()
+    })
+  })
+})
+
+t.test('server disconnect', function (t) {
+  var spawn = require('child_process').spawn
+  var node = process.execPath
+  var module = require.resolve('../')
+  var file = filename('server-disconnect')
+  var prog = 'var lock = require(process.argv[1])' +
+             '(process.argv[2])' +
+             '.then(console.log.bind(console,"1"))\n' +
+             'process.on("SIGHUP", lock.release.bind(lock))\n'
+  var child = spawn(node, [prog, module, file])
+  child.stdout.on('data', function () {
+    var didKill = false
+    setTimeout(function () {
+      child.kill('SIGHUP')
+    })
+    Promise.all([
+      slocket(file, onLock),
+      slocket(file, onLock)
+    ]).then(t.end)
+    function onLock (er, lock) {
+      if (!didKill) {
+        didKill = true
+        child.kill('SIGKILL')
+        setTimeout(lock.release, 100)
+      } else
+        lock.release()
+    }
+  })
+})
+
 t.test('open 1, connect 3, disconnect abruptly')
 t.test('server object emit error after being removed')
 t.test('try to lock on a non-socket, auto-lock once removed')
